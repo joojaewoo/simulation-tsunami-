f2dbd9a4fdea0910c9e3b82420feaca92a17a554
Jae-Woo Joo <wnwodn4293@naver.com>
2019-10-06 18:20:21 +0900

    방향값 수정

--- 주재우(해일모델링. java)/simul.java
+++ 주재우(해일모델링. java)/simul.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 public class simul {
 static public void main(String args[]) {
+	double goC=0.4;
+	double goS=0.3;
 	type [][] map=new type[50][50];
 	Random ran=new Random();
 	for (int i=0;i<5;i++) {
@@ -18,25 +20,25 @@ static public void main(String args[]) {
 		}
 		}
 	for(int i=0;i<50;i++) {
-		map[i][0].E=ran.nextDouble()*20;
+		map[i][0].E=ran.nextDouble()*200;
 		map[i][0].water=2;}
 	for(int i=0;i<49;i++){
 		System.out.println(i+1);
 		for(int j=0;j<50;j++) {
-			if(j==0) {map[j][i+1].E+=map[j][i].E*map[j][i+1].Friction/3;
-				map[j+1][i+1].E+=map[j][i].E*map[j][i+1].Friction/3;	
+			if(j==0) {map[j][i+1].E+=map[j][i].E*map[j][i+1].Friction*goC;
+				map[j+1][i+1].E+=map[j][i].E*map[j][i+1].Friction*goS;	
 				}
-				else if(j==49) {map[j][i+1].E+=map[j][i].E*map[j][i+1].Friction/3;
-				map[j-1][i+1].E+=map[j][i].E*map[j][i+1].Friction/3;	
+				else if(j==49) {map[j][i+1].E+=map[j][i].E*map[j][i+1].Friction*goC;
+				map[j-1][i+1].E+=map[j][i].E*map[j][i+1].Friction*goS;	
 				}
 				else {
-					map[j][i+1].E+=map[j][i].E*map[j][i+1].Friction/3;
-					map[j-1][i+1].E+=map[j][i].E*map[j][i+1].Friction/3;
-					map[j+1][i+1].E+=map[j][i].E*map[j][i+1].Friction/3;
+					map[j][i+1].E+=map[j][i].E*map[j][i+1].Friction*goC;
+					map[j-1][i+1].E+=map[j][i].E*map[j][i+1].Friction*goS;
+					map[j+1][i+1].E+=map[j][i].E*map[j][i+1].Friction*goS;
 				}
 		}
 		for(int j=0;j<50;j++) {
-			if(/*map[j][i].water==0&&*/map[j][i].E>0.01) map[j][i].water=2;
+			if(/*map[j][i].water==0&&*/map[j][i].E>0.1) map[j][i].water=2;
 		}
 		for(int x=0;x<50;x++){
 			for(int y=0;y<50;y++)
